f955b7d5dadbd8817ebe54c3981506f4026546b8
dead8309 <vaibhavmonu8309@gmail.com>
2024-08-14 14:12:02 +0530

    add prizes image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
-import Achievements from "@/components/achievements/Achievements";
-import { Defcon } from "@/components/defcon/Defcon";
 import Benefits from "@/components/events/Benefits";
-import Events from "@/components/events/Events";
 import Faq from "@/components/events/Faq";
 import ConnectWithUs from "@/components/events/ConnectWithUs";
 import Information from "@/components/events/Information";
 import Intro from "@/components/intro/Intro";
-import LogoSlider from "@/components/ui/LogoSlider";
+import Prizes from "@/components/prizes/Prizes";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10 md:gap-40">
+    <div className="flex flex-col gap-10 md:gap-32">
+      <Prizes />
       <Intro />
       <Information />
       <Benefits />
       <Faq />
       <ConnectWithUs />
-      {/* <LogoSlider /> */}
-      {/* <Defcon /> */}
-      {/* <Events /> */}
-      {/* <Achievements /> */}
     </div>
   );
 }
---
+++ src/components/prizes/Prizes.tsx
@@ -0,0 +1,18 @@
+const Prizes: React.FC = () => {
+  return (
+    <div id="prizes">
+      <div className="mt-4 px-2 2xl:pl-20 2xl:pr-28 w-full h-full relative overflow-hidden">
+        <img
+          src={"/assets/images/temp/prizes.png"}
+          alt="comic bg"
+          className="object-cover w-full border-[10px] border-black"
+        />
+        {/* <div className="polygon-wrapper"> */}
+        {/*   <div className="polygon-image"></div> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Prizes;
