ce1209d51f5f3eb0c599b9bb68b5956319c9a5fe
dead8309 <vaibhavmonu8309@gmail.com>
2024-08-15 01:32:21 +0530

    update app script url

--- src/components/leaderboard/LBNames.tsx
+++ src/components/leaderboard/LBNames.tsx
@@ -4,7 +4,7 @@ const THIRTY_MINUTES = 1800;
 const getData = async () => {
   try {
     const data = await fetch(
-      "https://script.google.com/macros/s/AKfycbxabUG7IHvUbHozpNyceTZGArdfFMQHl6qM1gdrCw8HiHuof3zEijO2CVSBOpgPTFP68g/exec",
+      "https://script.google.com/macros/s/AKfycbwLKTtu4lzDoQJ05Tp_IMtJPdSnrQo5Mi9mZp994_Mx77hRmaQAQR1ZdDh1frzFjGCbrA/exec",
       {
         next: { revalidate: THIRTY_MINUTES },
       }
@@ -21,7 +21,7 @@ const LBNames = async () => {
     <div className="  max-sm:h-[45%] no-scrollbar h-full overflow-auto flex flex-col rounded-lg p-2">
       {rankings.map((user: any, idx: number) => (
         <div
-          key={user.rank}
+          key={user.bitget_uid}
           className="flex gap-2 rounded-xl text-sm max-sm:text-xs m-1"
         >
           <div className="font-semibold px-3 sm:px-5 flex justify-center items-center rounded-xl bg-[#DA011F] text-white py-3 ">
@@ -31,7 +31,7 @@ const LBNames = async () => {
             {user.name}
           </div>
           <div className="font-semibold flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
-            uid-bonkNigga
+            {user.bitget_uid}
           </div>
           <div className="font-semibold px-3 sm:px-5 flex justify-center items-center rounded-xl bg-[#DA011F] text-white py-3">
             $900.00
