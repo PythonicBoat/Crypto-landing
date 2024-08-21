95a7e8e723f4c035728ddbb910d2d8055830b414
dead8309 <vaibhavmonu8309@gmail.com>
2024-08-22 00:22:21 +0530

    change ui for leaderboard

--- src/components/leaderboard/LBNames.tsx
+++ src/components/leaderboard/LBNames.tsx
@@ -2,42 +2,30 @@ import { WINNERS } from "@/data/winners";
 
 const LBNames = () => {
   const rankings = WINNERS;
-
-  const rankBlock = (start: number, end: number) => (
-    <div className="flex">
-      <div className="flex flex-col">
-        {rankings.slice(start, end).map((user) => (
-          <div
-            key={user.bitget_uid}
-            className="flex gap-2 shadow-2xl rounded-xl text-sm max-sm:text-xs m-1"
-          >
-            <div className="font-semibold border-2 border-black px-3 sm:px-5 flex justify-center items-center rounded-xl bg-[#DA011F] text-white py-3 ">
-              {user.ranks}
-            </div>
-            <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
-              {user.name}
-            </div>
-            <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
-              {user.bitget_uid}
-            </div>
-            <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
-              ₹{user.prize}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
-        {rankings[start].goodies}
-      </div>
-    </div>
-  );
-
   return (
     <div className="w-full md:w-fit overflow-x-auto z-20 border-2 border-black bg-fuchsia-900 flex flex-col rounded-lg px-2 py-4 md:px-8">
-      {rankBlock(0, 5)}
-      {rankBlock(5, 10)}
-      {rankBlock(10, 20)}
-      {rankBlock(20, 40)}
+      {rankings.map((user) => (
+        <div
+          key={user.bitget_uid}
+          className="flex gap-2 shadow-2xl rounded-xl text-sm max-sm:text-xs m-1"
+        >
+          <div className="font-semibold border-2 border-black px-3 sm:px-5 flex justify-center items-center rounded-xl bg-[#DA011F] text-white py-3 ">
+            {user.ranks}
+          </div>
+          <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
+            {user.name}
+          </div>
+          <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
+            {user.bitget_uid}
+          </div>
+          <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
+            ₹{user.prize}
+          </div>
+          <div className="font-semibold border-2 border-black flex-1 rounded-xl bg-[#DA011F] text-white px-4 py-2 flex justify-center items-center ">
+            {user.goodies}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
